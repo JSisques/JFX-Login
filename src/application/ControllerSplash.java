f7703b8f61664737ef55b1c53935207afa06e695
Javi <javierplazasisques@gmail.com>
2019-11-06 20:31:00 +0100

    Splash

---
+++ src/application/ControllerSplash.java
@@ -0,0 +1,125 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.FillTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+public class ControllerSplash {
+
+	@FXML
+	private AnchorPane rootPane;
+
+	@FXML
+	private ImageView imgLogo;
+
+	@FXML
+	private VBox loginContainer;
+
+	@FXML
+	private Label name;
+
+	@FXML
+	private Label eslogan;
+
+//	Main main;
+//	Stage stage;
+//
+//	public void main(Stage stage, Main main) {
+//		this.main = main;
+//		this.stage = stage;
+//	}
+
+	public void showLogin() {
+		try {
+			// Load the fxml file and create a new stage for the popup.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("main.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+			Stage homeStage = new Stage();
+			homeStage.setTitle("Main Menu");
+			Scene scene = new Scene(page);
+			homeStage.initStyle(StageStyle.UNDECORATED);
+			homeStage.setScene(scene);
+			homeStage.setMaximized(true);
+			homeStage.show();
+			System.out.println("second stage ok");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("second stage ko");
+		}
+	}
+
+	public void initialize() {
+
+		ScaleTransition ft = new ScaleTransition(Duration.seconds(0.1), imgLogo);
+		ft.setFromX(0);
+		ft.setFromY(0);
+		ft.play();
+		
+		ft.setOnFinished(eventooo -> {
+			ScaleTransition ft2 = new ScaleTransition(Duration.seconds(1), imgLogo);
+			ft2.setToX(1);
+			ft2.setToY(1);
+			ft2.play();
+			
+			ft2.setOnFinished(eventooo2 -> {
+				ScaleTransition st = new ScaleTransition(Duration.seconds(0.5), imgLogo);
+				st.setByX(0.3);
+				st.setByY(0.3);
+				st.play();
+
+				st.setOnFinished(evento1 -> {
+					ScaleTransition st1 = new ScaleTransition(Duration.seconds(0.5), imgLogo);
+					st1.setByX(-0.3);
+					st1.setByY(-0.3);
+					st1.play();
+
+					st1.setOnFinished(evento2 -> {
+						ScaleTransition st2 = new ScaleTransition(Duration.seconds(0.5), imgLogo);
+						st2.setByX(0.3);
+						st2.setByY(0.3);
+						st2.play();
+
+						st2.setOnFinished(evento3 -> {
+							ScaleTransition st3 = new ScaleTransition(Duration.seconds(0.5), imgLogo);
+							st3.setByX(-0.3);
+							st3.setByY(-0.3);
+							st3.play();
+
+							st3.setOnFinished(evento4 -> {
+								TranslateTransition ft3 = new TranslateTransition(Duration.seconds(2), imgLogo);
+								ft3.setByY(1000);
+								ft3.play();
+								
+								ft3.setOnFinished(evento5 -> {
+									Stage thisStage = (Stage) rootPane.getScene().getWindow();
+									thisStage.close();
+									showLogin();
+
+									System.out.println("Hecho");
+								});
+							});
+						});
+					});
+
+				});
+			});
+		});
+	}
+
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -6,16 +6,18 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("splash.fxml"));
 			AnchorPane root = (AnchorPane) loader.load();
-			Scene scene = new Scene(root, 800, 600);
-			primaryStage.initStyle(StageStyle.UNDECORATED);
+			Scene scene = new Scene(root, 400, 300);
+			scene.setFill(Color.TRANSPARENT);
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
